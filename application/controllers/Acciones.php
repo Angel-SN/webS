0c5f33de200d77fb16bcbd537f10ae7053d145be
Luis Angel SantN <luiangsn@gmail.com>
2017-08-31 11:29:23 -0500

    agrego htcacces, controlador Accciones por act

--- application/controllers/Acciones.php
+++ application/controllers/Acciones.php
@@ -1,52 +1,81 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Acciones extends CI_Controller {
-   public function __construct() {
-      parent:: __construct();
-      $this->load->helper('form');
-      $this->load->library('form_validation');
-      $this->load->model('RegistroUsuario');
-   }
+class Acciones extends CI_Controller
+{
+    public function __construct()
+    {
+        parent:: __construct();
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+        $this->load->model('RegistroUsuario');
+    }
 
-   public function index(){
-      $data = array('flag' => TRUE , 'mensaje' =>'hola' );
-      header('Content-Type: application/json');
-      echo json_encode($data);
-   }
+    public function index()
+    {
+        $data = array('flag' => true , 'mensaje' =>'hola' );
+        header('Content-Type: application/json');
+        echo json_encode($data);
+    }
 
-   public function registro(){
-      $_POST = json_decode(file_get_contents("php://input"), TRUE );
-      $this->form_validation->set_rules('MacAddress', 'Dirrecion Mac', 'required');
-      $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'required|numeric|is_unique[usuario.numero_cuenta]');
-      $this->form_validation->set_rules('Model', 'modelo', 'required');
-      if ($this->form_validation->run() == FALSE){
-         $data = array('flag' => FALSE);
-         header('Content-Type: application/json');
-         echo json_encode(array_merge($data,$this->form_validation->error_array()));
-      }
-      else {
-         $macAddress = $this->input->post('MacAddress');
-         $numCuenta = $this->input->post('NumCuenta');
-         $modelo = $this->input->post('Model');
-         $data = array('flag' => TRUE , 'mensaje' =>'Datos insertados correctamente' );
-         $data2 = $this->RegistroUsuario->registro($macAddress, $numCuenta, $modelo);
-         header('Content-Type: application/json');
-         echo json_encode(array_merge($data, $data2));
-      }
-   }
+    public function registro()
+    {
+        $_POST = json_decode(file_get_contents("php://input"), true);
+        $this->form_validation->set_rules('MacAddress', 'Dirrecion Mac', 'required|is_unique[dispositivo.mac]');
+        $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'required|numeric|is_unique[usuario.numero_cuenta]');
+        $this->form_validation->set_rules('Model', 'modelo', 'required');
+        if ($this->form_validation->run() == false) {
+            $data = array('flag' => false);
+            header('Content-Type: application/json');
+            echo json_encode(array_merge($data, $this->form_validation->error_array()));
+        } else {
+            $macAddress = $this->input->post('MacAddress');
+            $numCuenta = $this->input->post('NumCuenta');
+            $modelo = $this->input->post('Model');
+            $data = array('flag' => true , 'mensaje' =>'Datos insertados correctamente' );
+            $data2 = $this->RegistroUsuario->registro($macAddress, $numCuenta, $modelo);
+            header('Content-Type: application/json');
+            echo json_encode(array_merge($data, $data2));
+        }
+    }
 
-   public function agregar() {
-      if ($this->input->post()) {
-         $this->form_validation->set_rules('numero_cuenta', 'Numero de cuenta', 'required|numeric|is_unique[usuario.numero_cuenta]');
-         if ($this->form_validation->run() == FALSE){
+    public function agregar()
+    {
+        if ($this->input->post()) {
+            $this->form_validation->set_rules('numero_cuenta', 'Numero de cuenta', 'required|numeric|is_unique[usuario.numero_cuenta]');
+            if ($this->form_validation->run() == false) {
+                $this->load->view('view_form_usuario');
+            } else {
+                echo "informacion recibida";
+                print_r($this->input->post());
+            }
+        } else {
             $this->load->view('view_form_usuario');
-         }
-         else{
-            echo "informacion recibida";
-            print_r($this->input->post());
-         }
-      } else {
-         $this->load->view('view_form_usuario');
-      }
-   }
+        }
+    }
+
+    public function inicioSesion()
+    {
+        $_POST = json_decode(file_get_contents("php://input"), true);
+        $this->form_validation->set_rules('MacAddress', 'Dirrecion Mac', 'required');
+        $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'required|numeric');
+        $this->form_validation->set_rules('Fecha', 'Fecha', 'required');
+        if ($this->form_validation->run() == false) {
+            $data = array('flag' => false);
+            header('Content-Type: application/json');
+            echo json_encode(array_merge($data, $this->form_validation->error_array()));
+        } else {
+            $macAddress = $this->input->post('MacAddress');
+            $numCuenta = $this->input->post('NumCuenta');
+            $modelo = $this->input->post('Model');
+            $data = array('flag' => true , 'mensaje' =>'Datos insertados correctamente' );
+            $data2 = $this->RegistroUsuario->registro($macAddress, $numCuenta, $modelo);
+            header('Content-Type: application/json');
+            echo json_encode(array_merge($data, $data2));
+        }
+    }
 }
+
+
+/* End of file Acciones.php */
+/* Location: ./application/controllers/Acciones.php */
--- application/models/RegistroUsuario.php
+++ application/models/RegistroUsuario.php
@@ -1,64 +1,69 @@
 <?php
-class RegistroUsuario extends CI_Model {
-   private $res;
+class RegistroUsuario extends CI_Model
+{
+    private $res;
 
-   public function __construct() {
-      parent:: __construct();
-   }
+    public function __construct()
+    {
+        parent:: __construct();
+    }
 
-   private function insertarUsuario($numero_cuenta, $estado='1' ) {
-      $id = 0;
-      $datosUsuario = array('numero_cuenta' => $numero_cuenta,'estado'  => $estado);
-      $dat = $this->db->insert('usuario', $datosUsuario);
-      if($dat==true){
-         $this->db->select('idusuario');
-         $this->db->where('numero_cuenta',$numero_cuenta);
-         $id = $this->db->get('usuario');
-         return $id->row_array();
-         //return $id->row()->idusuario;
-      }
-      else{
-         return 0;
-      }
-   }
+    private function insertarUsuario($numero_cuenta, $estado='1')
+    {
+        $id = 0;
+        $datosUsuario = array('numero_cuenta' => $numero_cuenta,'estado'  => $estado);
+        $dat = $this->db->insert('usuario', $datosUsuario);
+        if ($dat == true) {
+            $this->db->select('idusuario');
+            $this->db->where('numero_cuenta', $numero_cuenta);
+            $id = $this->db->get('usuario');
+            return $id->row_array();
+            //return $id->row()->idusuario;
+        } else {
+            return 0;
+        }
+    }
 
-   private function insertarDispositivo($mac, $modelo, $estado='1' ) {
-      $id = 0;
-      $datosDispositivos = array(
+    private function insertarDispositivo($mac, $modelo, $estado='1')
+    {
+        $id = 0;
+        $datosDispositivos = array(
          'mac' => $mac,
          'modelo' => $modelo,
          'estado' => 1
       );
-      $dat = $this->db->insert('dispositivo', $datosDispositivos);
-      if($dat==true){
-         $this->db->select('iddispositivo');
-         $this->db->where('mac', $mac);
-         $id = $this->db->get('dispositivo');
-         return $id->row_array();
-         //return $id->row()->iddispositivo;
-      }
-      else{
-         return 0;
-      }
-   }
+        $dat = $this->db->insert('dispositivo', $datosDispositivos);
+        if ($dat == true) {
+            $this->db->select('iddispositivo');
+            $this->db->where('mac', $mac);
+            $id = $this->db->get('dispositivo');
+            return $id->row_array();
+            //return $id->row()->iddispositivo;
+        } else {
+            return 0;
+        }
+    }
 
-   public function registro($mac,$NoC,$modelo) {
-      $idUsuario = $this->insertarUsuario($NoC,1);
-      $idDispositivo = $this->insertarDispositivo($mac, $modelo,1 );
-      // $datosPosicion = array(
-      //    'latitud' => 0,
-      //    'longitud' => 0,
-      //    'fecha ' => '2107-08-17'
-      // );
-      // $this->db->insert('dispositivo', $datosDispositivos);
-      return array_merge($idUsuario, $idDispositivo);
-      // $datosRelacion = array(
+    public function registro($mac, $NoC, $modelo)
+    {
+        $idUsuario = $this->insertarUsuario($NoC, 1);
+        $idDispositivo = $this->insertarDispositivo($mac, $modelo, 1);
+        // $datosPosicion = array(
+        //    'latitud' => 0,
+        //    'longitud' => 0,
+        //    'fecha ' => '2107-08-17'
+        // );
+        // $this->db->insert('dispositivo', $datosDispositivos);
+        return array_merge($idUsuario, $idDispositivo);
+        // $datosRelacion = array(
       //    'usuario_idusuario' => ,
       //    'dispositivo_iddispositivo' => ,
       //    'fecha' => ,
       //    'estado' => ,
       //    'posicion_idposicion' => ,
       //
-   }
+    }
 }
-?>
+
+/* End of file RegistroUsuario.php */
+/* Location: ./application/models/RegistroUsuario.php */
