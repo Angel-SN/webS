f7c261e71a81cae6cf0ca3c18dca24c6774c8fe0
Luis Angel SantN <luiangsn@gmail.com>
2017-10-19 13:37:08 -0500

    Cambios en base a nuevo modelo de BD

--- application/controllers/Android_Cliente.php
+++ application/controllers/Android_Cliente.php
@@ -10,87 +10,119 @@ class Android_Cliente extends CI_Controller
         $this->load->model('Manejo_DB');
     }
 
+    public function usuarioExistente($str)
+    {
+        $result = ($this->Manejo_DB->buscarUsuario($str) != null) ? true : false ;
+        return $result;
+    }
+
     public function registro()
     {
         $_POST = json_decode(file_get_contents("php://input"), true);
         $this->form_validation->set_rules('MacAddress', 'Dirrecion Mac', 'trim|required|is_unique[dispositivo.mac]');
-        $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'trim|required|numeric|is_unique[usuario.numero_cuenta]');
+        $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'trim|required|numeric|is_unique[usuario.numero_usuario]');
         $this->form_validation->set_rules('Model', 'modelo', 'required');
         if ($this->form_validation->run() == false) {
             $data = array('flag' => false);
-            header('Content-Type: application/json');
-            echo json_encode(array_merge($data, $this->form_validation->error_array()));
+            $data = array_merge($data, $this->form_validation->error_array());
+            $this->output->set_status_header(400);
         } else {
             $macAddress = $this->input->post('MacAddress');
             $numCuenta = $this->input->post('NumCuenta');
             $modelo = $this->input->post('Model');
-            $reg = $this->Manejo_DB->registro($macAddress, $numCuenta, $modelo);
+            $reg = $this->Manejo_DB->registroUsuario($macAddress, $numCuenta, $modelo);
             $data = ($reg) ? array('flag' => true , 'mensaje' =>'Datos insertados correctamente' ) : array('flag' => false , 'mensaje' =>'error' );
-            header('Content-Type: application/json');
-            echo json_encode($data);
+            $this->output->set_status_header(201);
         }
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($data));
     }
 
-    public function inicioSesion($MacAddress, $NumCuenta)
+    public function inicioSesion($MacAddress, $numCuenta)
     {
         $in = array(
             'MacAddress' => $MacAddress,
-            'NumCuenta' => $NumCuenta
+            'NumCuenta' => $numCuenta
         );
         $this->form_validation->set_data($in);
         $this->form_validation->set_rules('MacAddress', 'Dirrecion Mac', 'trim|required');
-        $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'trim|required|numeric');
+        $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'trim|required|numeric|callback_usuarioExistente');
+        $this->form_validation->set_message('usuarioExistente','El usuario no Existe');
         if ($this->form_validation->run() == false) {
             $data = array('flag' => false);
-            header('Content-Type: application/json');
-            echo json_encode(array_merge($data, $this->form_validation->error_array()));
-        } else {
-            $data = $this->Manejo_DB->inicio($MacAddress, $NumCuenta);
-            header('Content-Type: application/json');
-            echo json_encode($data);
+            $data = array_merge($data, $this->form_validation->error_array());
+            $this->output->set_status_header(400);
+        }elseif($this->Manejo_DB->buscarSesion($numCuenta)->fin_sesion != null) {
+            $result = $this->Manejo_DB->inicioSesion($MacAddress, $numCuenta , "ruta");
+            $data = ($result) ? array('flag' => true,'mensaje' => 'Inicio de sesion correcto') : array('flag' => false , 'mensaje' =>'error en el proseso');
+            $this->output->set_status_header(200);
+        }else {
+            $data = array(
+                'flag' => false,
+                'mensaje' => "sesion ya abierta"
+            );
+            $this->output->set_status_header(400);
         }
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($data));
     }
 
-    public function cierreSesion($NumCuenta)
+    public function cierreSesion($numCuenta)
     {
         $in = array(
-            'NumCuenta' => $NumCuenta
+            'NumCuenta' => $numCuenta
         );
         $this->form_validation->set_data($in);
-        $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'trim|required|numeric');
+        $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'trim|required|numeric|callback_usuarioExistente');
+        $this->form_validation->set_message('usuarioExistente','El usuario no Existe');
         if ($this->form_validation->run() == false) {
             $data = array('flag' => false);
-            header('Content-Type: application/json');
-            echo json_encode(array_merge($data, $this->form_validation->error_array()));
-        } else {
-            $data = $this->Manejo_DB->cierre($NumCuenta);
-            header('Content-Type: application/json');
-            echo json_encode($data);
+            $data = array_merge($data, $this->form_validation->error_array());
+            $this->output->set_status_header(400);
+        }elseif($this->Manejo_DB->buscarSesion($numCuenta)->fin_sesion == null){
+            $result = $this->Manejo_DB->cierreSesion($numCuenta);
+            $data = ($result) ? array('flag' => true , 'mensaje' =>'Sesion cerrada' ) : array('flag' => false , 'mensaje' =>'error en el proseso');
+            $this->output->set_status_header(201);
         }
+        else {
+            $data = array(
+                'flag' => false,
+                'mensaje' => "Sesion ya cerrada"
+            );
+            $this->output->set_status_header(400);
+        }
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($data));
     }
 
-    public function actualizaPosicion()
+    public function insertarPosicion()
     {
         $_POST = json_decode(file_get_contents("php://input"), true);
         $this->form_validation->set_rules('Latitud', 'Latitud', 'trim|required');
         $this->form_validation->set_rules('Longitud', 'Longitud', 'trim|required');
-        $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'trim|required|numeric');
+        $this->form_validation->set_rules('NumCuenta', 'Numero de cuenta', 'trim|numeric|required|callback_usuarioExistente');
+        $this->form_validation->set_message('usuarioExistente','El usuario no Existe');
         if ($this->form_validation->run() == false) {
             $data = array('flag' => false);
-            header('Content-Type: application/json');
-            echo json_encode(array_merge($data, $this->form_validation->error_array()));
+            $data = array_merge($data, $this->form_validation->error_array());
+            $this->output->set_status_header(400);
         } else {
             $numCuenta = $this->input->post('NumCuenta');
             $latitud = $this->input->post('Latitud');
             $longitud = $this->input->post('Longitud');
-            $actPos = $this->Manejo_DB->actualizarPosicion($numCuenta, $latitud, $longitud);
-            $data = ($actPos) ? array('flag' => true , 'mensaje' =>'posicion Actualizada' ) : array('flag' => false , 'mensaje' =>'error' );
-            header('Content-Type: application/json');
-            echo json_encode($data);
+            $idSesion = $this->Manejo_DB->buscarSesion($numCuenta)->idsesion;
+            $inPos = $this->Manejo_DB->insertarPosicion($latitud,$longitud,$idSesion);
+            $data = ($inPos != 0) ? array('flag' => true , 'mensaje' =>'posicion enviada' ) : array('flag' => false , 'mensaje' =>'error en el proseso');
+            $this->output->set_status_header(201);
         }
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($data));
     }
 }
-
 
 /* End of file Android_Cliente.php */
 /* Location: ./application/controllers/Android_Cliente.php */
--- application/models/Manejo_DB.php
+++ application/models/Manejo_DB.php
@@ -3,52 +3,87 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Manejo_DB extends CI_Model
 {
-
     public function __construct()
     {
         parent:: __construct();
     }
 
-    private function obtenerFecha()
+    public function obtenerFecha()
     {
+        date_default_timezone_set("America/Mexico_City");
         $hoy = getdate();
-        return $hoy["year"]."-".$hoy["mon"]."-".$hoy["mday"];
+        return $hoy["year"]."-".$hoy["mon"]."-".$hoy["mday"]." ".$hoy["hours"].":".$hoy["minutes"].":".$hoy["seconds"];
     }
 
-    private function insertarUsuario($numero_cuenta, $estado='Activo')
+    private function insertarUsuario($numero_usuario, $descripción_usuario = null)
     {
-        $datosUsuario = array('numero_cuenta' => $numero_cuenta,'estado'  => $estado);
-        $dat = $this->db->insert('usuario', $datosUsuario);
-        if ($dat == true) {
+        $datosUsuario = array(
+            'numero_usuario' => $numero_usuario,
+            'descripción_usuario'  => $descripción_usuario
+        );
+        $data = $this->db->insert('usuario', $datosUsuario);
+        if ($data == true) {
             return $this->db->insert_id();
         } else {
             return 0;
         }
     }
 
-    private function insertarDispositivo($mac, $modelo, $estado='Activo')
+    private function insertarDispositivo($mac, $modelo, $idUsuario)
     {
         $datosDispositivos = array(
             'mac' => $mac,
             'modelo' => $modelo,
-            'estado' => $estado
+            'usuario_idusuario' => $idUsuario
         );
-        $dat = $this->db->insert('dispositivo', $datosDispositivos);
-        if ($dat == true) {
+        $data = $this->db->insert('dispositivo', $datosDispositivos);
+        if ($data == true) {
             return $this->db->insert_id();
         } else {
             return 0;
         }
     }
 
-    public function insertarPosicion($lat = null, $long = null)
+    public function insertarPosicion($lat = null, $long = null , $idSesion)
     {
         $datosPosicion = array(
            'latitud' => $lat,
            'longitud' => $long,
-           'fecha ' => $this->obtenerFecha()
+           'fecha ' => $this->obtenerFecha(),
+           'sesion_idsesion' => $idSesion
         );
-        $dat = $this->db->insert('posicion', $datosPosicion);
+        $data = $this->db->insert('posicion', $datosPosicion);
+        if ($data == true) {
+            return $this->db->insert_id();
+        } else {
+            return 0;
+        }
+    }
+
+    public function insertarRuta($nombre){
+        $datosRuta = array(
+            'nombre' => $nombre,
+        );
+        $data = $this->db->insert('ruta', $datosRuta);
+        if ($data == true) {
+            return $this->db->insert_id();
+        }
+        else{
+            return 0;
+        }
+
+    }
+
+    private function crearSesion($idUsuario = '0', $idRuta = '0')
+    {
+        $datosSesion = array(
+            'usuario_idusuario' => $idUsuario,
+            'inicio_sesion' => $this->obtenerFecha(),
+            'fin_sesion' => null,
+            'estado' => 'Inactivo',
+            'ruta_ruta' => $idRuta
+        );
+        $dat = $this->db->insert('sesion', $datosSesion);
         if ($dat == true) {
             return $this->db->insert_id();
         } else {
@@ -56,105 +91,58 @@ class Manejo_DB extends CI_Model
         }
     }
 
-    private function crearRelacion($idU = '0', $idD ='0', $idP = '0')
-    {
-        $datosRelacion = array(
-            'usuario_idusuario' => $idU,
-            'dispositivo_iddispositivo' => $idD,
-            'fecha' => $this->obtenerFecha(),
-            'estado' => 'Inactivo',
-            'posicion_idposicion' => $idP
-        );
-        $dat = $this->db->insert('sesion', $datosRelacion);
-        if ($dat == true) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    public function registro($mac, $NoC, $modelo)
+    public function registroUsuario($mac, $NoC, $modelo)
     {
         $idUsuario = $this->insertarUsuario($NoC);
-        $idDispositivo = $this->insertarDispositivo($mac, $modelo);
-        $idPosicion = $this->insertarPosicion();
-        if ($idUsuario != 0 && $idDispositivo != 0 && $idPosicion != 0) {
-            $rel = $this->crearRelacion($idUsuario, $idDispositivo, $idPosicion);
-            return $rel;
-        } else {
-            return false;
-        }
-    }
-
-    public function actualizarPosicion($noC, $lat, $long)
-    {
-        $query = $this->db->query("select p.idposicion from posicion p inner join sesion s on p.idposicion = s.posicion_idposicion inner join usuario u on s.usuario_idusuario = u.idusuario where u.numero_cuenta = '$noC';");
-        if ($query->row() != null) {
-            $datosPosicion = array(
-               'latitud' => $lat,
-               'longitud' => $long,
-               'fecha ' => $this->obtenerFecha()
-            );
-            $this->db->where('idposicion', $query->row()->idposicion);
-            $this->db->update('posicion', $datosPosicion);
+        $idDispositivo = $this->insertarDispositivo($mac, $modelo, $idUsuario);
+        if($idUsuario != 0 && $idDispositivo != 0){
             return true;
-        } else{
+        }
+        else{
             return false;
         }
-    }
-
-    public function inicio($mac, $noC)
-    {
-        $query = $this->db->query('select * from dispositivo d inner join sesion s on d.iddispositivo = s.dispositivo_iddispositivo inner join usuario u on s.usuario_idusuario = u.idusuario where u.numero_cuenta = "'.$noC.'" and d.mac = "'.$mac.'";');
-        if ($query->row() != null) {
-            $respuesta = array(
-                'flag' => true,
-                'mensaje' => 'Inicio de secion correcto'
-            );
-            $data = array(
-                'fecha' => $this->obtenerFecha(),
-                'estado' => 'Activo'
-            );
-            $this->db->where('idsesion', $query->row()->idsesion);
-            $this->db->update('sesion', $data);
-        } else {
-            $respuesta = array(
-                'flag' => false,
-                'mensaje' => 'Usuario no registrado'
-            );
-        }
-        return $respuesta;
-    }
-
-    public function cierre($noC)
-    {
-        $query = $this->db->query('select * from dispositivo d inner join sesion s on d.iddispositivo = s.dispositivo_iddispositivo inner join usuario u on s.usuario_idusuario = u.idusuario where u.numero_cuenta = "'.$noC.'";');
-        if ($query->row() != null) {
-            $respuesta = array(
-                'flag' => true,
-                'mensaje' => 'Fin de Sesion'
-            );
-            $data = array(
-                'estado' => 'Inactivo'
-            );
-            $this->db->where('idsesion', $query->row()->idsesion);
-            $this->db->update('sesion', $data);
-        } else {
-            $respuesta = array(
-                'flag' => false,
-                'mensaje' => 'error en el Proceso'
-            );
-        }
-        return $respuesta;
     }
 
     public function buscarUsuario($noC)
     {
-        $this->db->where('numero_cuenta', $noC);
-        $query = $this->db->get('datos_usuario');
-        return $query->row_array();
+        $this->db->select('idusuario');
+        $this->db->from('usuario');
+        $this->db->join('dispositivo', 'usuario.idusuario = dispositivo.usuario_idusuario', 'inner');
+        $this->db->where('usuario.numero_usuario', $noC);
+        $Usuario = $this->db->get();
+        return $Usuario->row();
     }
 
+    public function buscarSesion($numero_usuario)
+    {
+        $this->db->where('usuario_idusuario', $this->buscarUsuario($numero_usuario)->idusuario);
+        $query = $this->db->get('sesion');
+        return $query->last_row();
+    }
+
+    public function inicioSesion($mac, $noC ,$nombreRuta)
+    {
+        $idUsuario = $this->buscarUsuario($noC)->idusuario;
+        $this->db->select('ruta');
+        $this->db->where('nombre', $nombreRuta);
+        $idRuta = $this->db->get('ruta')->row()->ruta;
+        $sesion = $this->crearSesion($idUsuario,$idRuta);
+        $respuesta = ($sesion != 0) ? true : false ;
+        return $respuesta;
+    }
+
+    public function cierreSesion($noC)
+    {
+        $idSesion = $this->buscarSesion($noC)->idsesion;
+        $data = array(
+            'fin_sesion' => $this->obtenerFecha(),
+            'estado' => "Inactivo"
+        );
+        $this->db->where('idsesion', $idSesion);
+        $query = $this->db->update('sesion', $data);
+        return $query;
+    }
 }
 
 /* End of file Manejo_DB.php */
